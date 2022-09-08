d1620797a576f322e680d87dc51058674c5538da
Andrey Manzadey <andrey.manzadey@gmail.com>
2022-09-09 01:59:12 +0300

    Create StorageLogsMenu.php

---
+++ src/Orchid/Actions/Menu/StorageLogsMenu.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Manzadey\LaravelOrchidStorageLog\Orchid\Actions\Menu;
+
+use Orchid\Screen\Actions\Menu;
+
+class StorageLogsMenu
+{
+    public static function make() : Menu
+    {
+        return Menu::make(__('Системные логи'))
+            ->icon('settings')
+            ->route('platform.storage-logs.list');
+    }
+}
